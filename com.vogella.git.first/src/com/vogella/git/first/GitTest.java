c0bfcec3714d5a8cca44a42595c2b68e11b6c816
Costis Aivalis <costis.aivalis@gmail.com>
2024-03-04 20:47:27 +0200

    after token

--- com.vogella.git.first/src/com/vogella/git/first/GitTest.java
+++ com.vogella.git.first/src/com/vogella/git/first/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Git is nice, cool and useful!");
 		System.out.println("Noch eine  Z eile output ");
+		System.out.println("A Token used!");
 	}
 }
