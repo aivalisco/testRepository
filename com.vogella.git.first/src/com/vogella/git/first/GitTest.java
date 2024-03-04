fbc7fe829f6a211b483d3ffb0eb36f4d46fa3ac5
Costis Aivalis <costis.aivalis@gmail.com>
2024-03-04 15:57:51 +0200

    Zweite out!

--- com.vogella.git.first/src/com/vogella/git/first/GitTest.java
+++ com.vogella.git.first/src/com/vogella/git/first/GitTest.java
@@ -3,5 +3,6 @@ package com.vogella.git.first;
 public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Git is nice, cool and useful!");
+		System.out.println("Noch eine Zeile output");
 	}
 }
